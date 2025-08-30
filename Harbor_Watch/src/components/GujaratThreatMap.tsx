f3c04aea9eea6708d8676292d40102a05a728e58
Vismay Shah <vismayshah2176@gmail.com>
2025-08-31 01:32:10 +0530

    .

--- Harbor_Watch/src/components/GujaratThreatMap.tsx
+++ Harbor_Watch/src/components/GujaratThreatMap.tsx
@@ -25,7 +25,7 @@ const GujaratThreatMap = ({ threats, onLocationClick }: GujaratThreatMapProps) =
 
 
   const gujaratLocations = [
-  { name: 'Varvala',      coordinates: [69.6, 21.7], type: 'coastal_town' },     // lowered south
+  { name: 'Varvala',      coordinates: [69.6, 21.3], type: 'coastal_town' },     // lowered south
   { name: 'Shivrajpur',   coordinates: [70.2, 21.9], type: 'coastal_town' },     // moved east & south
   { name: 'Dwarka',       coordinates: [69.6, 21.4], type: 'coastal_town' },     // moved east
   { name: 'Okha',         coordinates: [69.5, 22.8], type: 'cityport' },         // keep same for now
@@ -33,8 +33,6 @@ const GujaratThreatMap = ({ threats, onLocationClick }: GujaratThreatMapProps) =
   { name: 'Positra',      coordinates: [71.2, 22.0], type: 'coastal_town' },     // already correct
   { name: 'Surajkaradi',  coordinates: [70.2, 21.0], type: 'fishing_harbor' },   // moved south
 ];
-
-
 
 
   const getThreatColor = (level: string) => {
